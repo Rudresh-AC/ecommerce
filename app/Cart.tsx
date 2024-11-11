67eb20043a67837176ba6ea931f1e99fa805cbb2
Rudresh A C <rudreshac@Rudreshs-MacBook-Air.local>
2024-11-11 22:20:08 +0530

    first commit

---
+++ app/Cart.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Cart() {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Cart screen</Text>
+    </View>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text, FlatList } from "react-native";
+import React from "react";
+import products from "../assets/products.json";
+import ProductListItem from "../components/ProductListItem";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+      />
+    </View>
+  );
+}
